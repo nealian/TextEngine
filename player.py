9d67873da005ce9ad1f1875d00295e5e6b1a70f3
Tamwyn559 <tamwyn559@users.noreply.github.com>
2015-09-04 21:35:00 -0600

    Initial update of a really unfinished Player class. Still need to figure out what to store location at for the player.

---
+++ player.py
@@ -0,0 +1,12 @@
+__author__ = "wmyers559"
+
+class Player(object):
+
+	def __init__(self, health)
+		self.location = ""  #Not sure what this should be
+		self.health = health
+		self.inventory = {}
+		self.equiptment = {}
+		self.stats = {}
+
+	def 
