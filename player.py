108f4ebf957e6f4df210ea5977f1419ec04d7680
Tamwyn559 <tamwyn559@users.noreply.github.com>
2015-09-05 00:40:42 -0600

    More improvement with player.py. Needs more work before it is functional.

--- player.py
+++ player.py
@@ -3,10 +3,10 @@ __author__ = "wmyers559"
 import entity
 
 class Player(object):
-    """ """
+    """Basic player object. Takes initial health as a argument."""
 
     def __init__(self, health):
-        self.location = ""  # Not sure what this should be
+        self.location = ""  # Not sure what this should be yet
         self.health = health
         self.inventory = {}
         self.equiptment = {}
@@ -19,12 +19,25 @@ class Player(object):
         pass
 
     def examine(self, subject):
-        pass
+    """Get the description of an object."""
+        print(subject.description_verbose)
 
     def use(self, item1, item2):
         pass
 
     def open(self, item):
+    """Checks to see if an object is openable and then tries to open it."""
+        if (item.openable == True)
+            print(item.#Whatever we want to name the method.)
+
+        else
+            raise Exception("Invalid keyword")
+
+    def interact(self, item):
+        #item.action()  Maybe??
         pass
 
-    
+    def damage(self, damage):
+    """Subtracts the amount of damage(@param damage) done from the health of the player."""
+        self.health -= damage
+
